9f307cdb84b94fc451f0df10555d6e9045a525fb
tores-up <toreis@reisan.dev>
2024-11-06 11:38:53 +0900

    chore: install Vuetify

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
@@ -9,5 +11,19 @@ export default defineNuxtConfig({
   },
 
   srcDir: "src/",
-  modules: ["@nuxtjs/storybook"]
+  modules: [(_options, nuxt) => {
+    nuxt.hooks.hook('vite:extendConfig', (config) => {
+      config.plugins.push(vuetify({autoImport: true}))
+    })
+  },"@nuxtjs/storybook"],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
+  }
 })
