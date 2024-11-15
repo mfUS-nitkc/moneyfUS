f7ee026143aff8df590f6486800cf1d24014f9e0
とれいす <65478744+toreis-up@users.noreply.github.com>
2024-11-16 01:52:31 +0900

    init(backend): init Django (#3)
    * init(backend): init Django
    
    not setup mysql connector yet.
    
    * chore: install django + mysql
    
    * chore: docker prod

---
+++ backend/moneyFUS/views/data.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def data(request):
+    data = [
+            {
+                "id": 1,
+                "date": "2024-10-21",
+                "content": "TESTTEST"
+                }
+            ]
+    return JsonResponse(data, safe=False)
