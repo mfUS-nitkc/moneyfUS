0e6b73226c30215fbb5607e0cb5af4e45fdd440f
toreis-up <reisan03@reisan.dev>
2024-12-13 21:49:05 +0900

    chore(back) breaking change: add create_at, updated_at field to user model

--- backend/moneyFUS/models/user/user.py
+++ backend/moneyFUS/models/user/user.py
@@ -31,6 +31,8 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
