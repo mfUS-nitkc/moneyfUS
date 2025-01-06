51519d82f66b6047bf198f956d93b085bcabfe17
tores-up <reisan03@reisan.dev>
2025-01-06 10:50:31 +0900

    chore: connect SQLite when test is executed

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -1,3 +1,5 @@
 from .base import *  # noqa: F403
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ["127.0.0.1", "localhost"])  # noqa: F405
+ALLOWED_HOSTS: list[str] = os.getenv("ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")  # noqa: F405
+
+DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"  # noqa: F405
