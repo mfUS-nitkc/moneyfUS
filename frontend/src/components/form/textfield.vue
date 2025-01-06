729d627c47941c36340909e8a006fc1853530d56
eatingpasuta <seiya20050221@gmail.com>
2025-01-06 11:56:56 +0900

    レビューをGPTに投げました

--- frontend/src/components/form/textfield.vue
+++ frontend/src/components/form/textfield.vue
@@ -1,3 +1,33 @@
 <template>
-    <v-text-field label=""></v-text-field>
-</template>
+    <v-text-field
+      :label="label"
+      v-model="inputValue"
+    ></v-text-field>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        required: true,
+      },
+      modelValue: {
+        type: String,
+        required: false,
+        default: "",
+      },
+    },
+    computed: {
+      inputValue: {
+        get() {
+          return this.modelValue;
+        },
+        set(value) {
+          this.$emit("update:modelValue", value);
+        },
+      },
+    },
+  };
+  </script>
+  
