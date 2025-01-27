bfba6037a56fadc5f553c1894350ac08465e41ca
Asai <j20401@inc.kisarazu.ac.jp>
2025-01-27 11:57:24 +0900

    チェックボックスを作ったよ．

---
+++ frontend/src/components/checkbox/checkbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid>
+    <p>{{ selected }}</p>
+    <v-checkbox v-model="selected" label="1" value="1"></v-checkbox>
+    <v-checkbox v-model="selected" label="2" value="2"></v-checkbox>
+    <v-checkbox v-model="selected" label="3" value="3"></v-checkbox>
+    <v-checkbox v-model="selected" label="4" value="4"></v-checkbox>
+    <v-checkbox v-model="selected" label="5" value="5"></v-checkbox>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        selected: ['1'],
+      }
+    },
+  }
+</script>
