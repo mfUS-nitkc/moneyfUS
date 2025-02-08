fbbf5d4ea84365efd5803b8d10a4d21d5a154182
eatingpasuta <107804277+eatingpasuta@users.noreply.github.com>
2025-02-08 09:54:28 +0900

    feat: formのtextfield (#28)
    * feat: formのtextfield
    
    * レビューをGPTに投げました
    
    * fix: textfield
    
    * feat: implement usage of textfield
    
    ---------
    
    Co-authored-by: tores-up <reisan03@reisan.dev>

---
+++ frontend/src/components/form/textfield.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>{{ label }}</div>
+    <v-text-field
+    :label="label"
+    v-model="internalValue"
+    :rules="rules"
+    outlined
+    ></v-text-field>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, defineProps, defineEmits } from 'vue'
+
+const props = defineProps<{
+  label?: string
+  value: string | number
+  rules?: Array<(value: any) => boolean | string>
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:value', value: string | number): void
+}>()
+
+const internalValue = computed({
+  get: () => props.value,
+  set: (value: string | number) => emit('update:value', value)
+})
+</script>
+
+<style scoped>
+</style>
