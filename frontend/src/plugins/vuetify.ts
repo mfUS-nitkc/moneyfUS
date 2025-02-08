7566c32340ad51867ef8744450212c197814b977
tores-up <reisan03@reisan.dev>
2025-02-08 17:58:20 +0900

    feat: add color theme

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -2,11 +2,22 @@
 import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
+import colors from 'vuetify/util/colors'
 import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    theme: {
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: colors.blue.lighten4,
+            secondary: colors.teal.lighten1,
+          }
+        },
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })
