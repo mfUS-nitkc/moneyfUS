e6f642af8ae999f159a55a5352272127e1bb3955
とれいす <65478744+toreis-up@users.noreply.github.com>
2025-02-10 17:13:55 +0900

    feat: lend endpoint (#38)

--- backend/moneyFUS/models/asset/usage_category.py
+++ backend/moneyFUS/models/asset/usage_category.py
@@ -4,3 +4,4 @@ import uuid
 class UsageCategory(models.Model):
     category_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     category_name = models.CharField(max_length=255)
+    category_code = models.CharField(max_length=50, unique=True, null=True, blank=True)
