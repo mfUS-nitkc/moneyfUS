bdd5efa177ee0934b4cb3672c94c7a3f1e0b460b
tores-up <reisan03@reisan.dev>
2025-02-11 22:29:22 +0900

    feat: borrowed

--- backend/moneyFUS/urls.py
+++ backend/moneyFUS/urls.py
@@ -5,6 +5,7 @@ from .views.asset.index import AssetView
 from .views.asset.category import UsageCategoryView
 from .views.loan.lend import LendView
 from .views.loan.index import LoanCheckoutView
+from .views.loan.borrowed import BorrowedView
 
 urlpatterns = [
     path("user/login", LoginView.as_view()),
@@ -12,5 +13,6 @@ urlpatterns = [
     path("asset", AssetView.as_view()),
     path("asset/category", UsageCategoryView.as_view()),
     path("lend", LendView.as_view()),
+    path("borrowed", BorrowedView.as_view()),
     path("lend/checkout/<str:loan_id>", LoanCheckoutView.as_view()),
 ]
