1bf1c6656add5a0f6bd891013e17e12c94596294
tores-up <reisan03@reisan.dev>
2025-02-12 22:54:09 +0900

    refactor: UserSelfView

--- backend/moneyFUS/views/user/index.py
+++ backend/moneyFUS/views/user/index.py
@@ -39,33 +39,3 @@ class UserView(APIView):
             {"success": False, "reason": "Invalid credentials"},
             status=status.HTTP_400_BAD_REQUEST,
         )
-
-    def delete(self, request):
-        user = request.user
-        user.is_active = False
-        user.save()
-        res = Response({"success": True}, status=status.HTTP_204_NO_CONTENT)
-        res.delete_cookie("token")
-        return res
-
-    def patch(self, request):
-        user = request.user
-        user.email = request.data.get("email")
-        user.username = request.data.get("username")
-        old_plain_password = request.data.get("old_password")
-        new_plain_password = request.data.get("new_password")
-
-        res = Response()
-
-        try:
-            user.save()
-            res = Response({"success": True})
-        except ValidationError:
-            res = Response({"sucess": False})
-            return res
-
-        if new_plain_password and user.check_password(old_plain_password):
-            user.set_password(new_plain_password)
-            res = Response({"success": True})
-
-        return res
---
+++ backend/moneyFUS/views/user/self.py
@@ -0,0 +1,42 @@
+from django.forms import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from ...backends.user.cookie_authentication import CookieTokenBackend
+
+
+class UserSelfView(APIView):
+  authentication_classes = [CookieTokenBackend]
+  permission_classes = [IsAuthenticated]
+  
+  def delete(self, request):
+    user = request.user
+    user.is_active = False
+    user.save()
+    res = Response({"success": True}, status=status.HTTP_204_NO_CONTENT)
+    res.delete_cookie("token")
+    return res
+
+  def patch(self, request):
+      user = request.user
+      user.email = request.data.get("email")
+      user.username = request.data.get("username")
+      old_plain_password = request.data.get("old_password")
+      new_plain_password = request.data.get("new_password")
+
+      res = Response()
+
+      try:
+          user.save()
+          res = Response({"success": True})
+      except ValidationError:
+          res = Response({"sucess": False})
+          return res
+
+      if new_plain_password and user.check_password(old_plain_password):
+          user.set_password(new_plain_password)
+          res = Response({"success": True})
+
+      return res
