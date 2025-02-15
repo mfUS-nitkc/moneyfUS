e1f810152a5396cdccc0e58892419e5f1de329b7
tores-up <reisan03@reisan.dev>
2025-02-15 11:56:59 +0900

    feat: user store
    api endpoint

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,11 @@ import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:8000/api/v1',
+    }
+  },
   alias: {
     "~": "./src",
     "public": "./public"
